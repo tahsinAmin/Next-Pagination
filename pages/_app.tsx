63285f61b74cedb352f5e8d72bea347f74b2cc4b
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-11-11 20:25:14 +0600

    Connect NProgress and Next.js Router Events

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,21 @@
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from 'next/app'
 import {Router} from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+NProgress.configure({ showSpinner: false }); {/**Not showing the spinner at the right */}
 
 Router.events.on('routeChangeStart', () => {
-  console.log('on start');
+  NProgress.start()
 })
 
 Router.events.on('routeChangeComplete', () => {
-  console.log('on complete');
+  NProgress.done()
 })
 
 Router.events.on('routeChangeError', () => {
-  console.log('on error');
+  NProgress.done()
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
